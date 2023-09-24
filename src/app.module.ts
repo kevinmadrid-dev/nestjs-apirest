9cc7f9f441b3b29ddb45015f608598e4bc157811
K3yJey <kevin_madrid10@hotmail.es>
2023-09-24 18:13:03 -0500

    fix: :bug: Se corrigió un error de acceso a la DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,26 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MessageController } from './message/message.controller';
+import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { MessageController } from "./message/message.controller"
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "user",
-      password: "app",
-      database: "sendapp_db",
-      entities: [__dirname + "/**/*.entity{.ts,.js}"],
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: "mysql",
+        host: "localhost",
+        port: 3306,
+        username: "user",
+        password: "123456",
+        database: "sendapp_db",
+        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        synchronize: true,
+        logging: true
+      })
     })
   ],
   controllers: [AppController, MessageController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
